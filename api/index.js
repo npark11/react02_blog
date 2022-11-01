b70b2e7850965faa4007d6f8a9469bbf93f458e4
Nari Park <npark11@myseneca.ca>
2022-10-31 23:52:54 -0400

    added register route

--- api/index.js
+++ api/index.js
@@ -2,16 +2,18 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+const authRoute = require('./routes/auth');
 
 dotenv.config();
+app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URL, { 
-    useNewUrlParser: true, 
-    useUnifiedTopology: true,
-    useCreateIndex: true
+    useNewUrlParser: true
 })
 .then(console.log("Connected to MongoDB"))
 .catch((error) => console.log(err));
+
+app.use('/api/auth', authRoute);
 
 app.listen("5000", () => {
     console.log("Backend is running");
