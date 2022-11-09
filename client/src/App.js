2d4e6c35c4080f18cf54c92074a6846ca0fa9473
Nari Park <npark11@myseneca.ca>
2022-11-09 09:06:01 -0500

    created login system using Context API

--- client/src/App.js
+++ client/src/App.js
@@ -7,9 +7,11 @@ import Login from './pages/login/Login';
 import Register from './pages/register/Register';
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useContext } from 'react';
+import { Context } from './context/Context';
 
 function App() {
-  const user = false;
+  const { user } = useContext(Context);
   return (
     <BrowserRouter>
     <Navbar />
--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,16 +1,47 @@
+import { useRef, useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { Context } from '../../context/Context';
+import axios from 'axios';
 import './login.css';
 
 export default function Login() {
+  
+  const userRef = useRef();
+  const passwordRef = useRef();
+  const { dispatch, isFetching } = useContext(Context)
+  
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    dispatch({ type:"LOGIN_START" });
+    try {
+      const res = await axios.post("/auth/login", {
+        username: userRef.current.value,
+        password: passwordRef.current.value,
+      })
+      dispatch({ type:"LOGIN_SUCCESS", payload: res.data });
+    } catch(err) {
+      dispatch({ type:"LOGIN_FAILURE" });
+    }
+  };
+
   return (
     <div className="login">
         <span className="login__title">Login</span>
-        <form className="login__form">
-            <label>Email</label>
-            <input type="email" placeholder="Enter your email" />
+        <form className="login__form" onSubmit={handleSubmit}>
+            <label>Username</label>
+            <input 
+              type="text" 
+              placeholder="Enter your username"
+              ref={userRef}
+            />
+
             <label>Password</label>
-            <input type="password" placeholder="Enter your password" />
-            <button className="login__button">Login</button>
+            <input 
+              type="password" 
+              placeholder="Enter your password" 
+              ref={passwordRef}
+            />
+            <button className="login__button" type="submit" disabled={isFetching}>Login</button>
         </form>
         <button className="login__register__button">
           <Link className="link" to="/register">Register</Link>
